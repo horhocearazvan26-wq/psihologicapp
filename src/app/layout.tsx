62b185f93392c7a4da23d5f00f60a02edd9d1c8e
Razvan Horhocea <horhocearazvan26@gmail.com>
2026-04-14 17:04:48 +0300

    Disable mobile zoom for native app feel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   themeColor: '#f7f8f6',
 }
 
